e27983125ad21a890e48acd0ffcbd8f794562772
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2024-02-05 17:47:20 +0200

    add alternate Singleton implementation in comment

--- singleton/src/javaScriptUsage.js
+++ singleton/src/javaScriptUsage.js
@@ -10,3 +10,59 @@ const badSingleton = new BadSingleton()
 const badSingleton2 = new BadSingleton()
 
 console.log('Same instance (bad)? ' + (badSingleton === badSingleton2))
+
+/*
+// Alternate Singleton implementation
+
+let instance
+let count = 0
+
+class Counter {
+  constructor() {
+    if (instance) {
+      throw new Error("You can only create one instance!");
+    }
+    instance = this;
+  }
+  
+  getInstance() {
+    return this;
+  }
+
+  getCount() {
+    return counter;
+  }
+
+  increment() {
+    return ++counter;
+  }
+
+  decrement() {
+    return --counter;
+  }
+}
+
+// freeze the singleton instance so that it cannot be modified
+const singletonCounter = Object.freeze(new Counter());
+export default singletonCounter;
+*/
+
+/*
+// Alternate Singleton implementation 2
+
+let count = 0;
+
+const counter = {
+  increment() {
+    return ++count;
+  },
+
+  decrement() {
+    return --count;
+  }
+};
+
+
+Object.freeze(counter);
+export { counter };
+*/
